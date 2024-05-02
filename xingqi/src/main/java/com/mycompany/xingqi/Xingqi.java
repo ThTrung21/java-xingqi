8c80a355311c737651d34424371a857b2e9a0da9
ThTrung_ <ntrung1121@gmail.com>
2024-05-02 10:31:31 +0700

    test new ide

---
+++ xingqi/src/main/java/com/mycompany/xingqi/Xingqi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.xingqi;
+
+/**
+ *
+ * @author Trung
+ */
+public class Xingqi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
